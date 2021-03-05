a8e505327d69027a7acda5eb5dd6f1b8ae00acce
wesolyigor <wesolowski.igor@gmail.com>
2021-03-05 18:57:22 +0100

    add views, login to drf

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
