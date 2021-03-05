36965ed354718f1f6d978ef629f4540ab49dc597
wesolyigor <wesolowski.igor@gmail.com>
2021-03-05 19:21:56 +0100

    add read only auth

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import permissions
 from rest_framework.generics import RetrieveAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from posts.models import Post
+from posts.permissions import IsAuthorOrReadOnly
 from posts.serializers import PostSerializer
 
 
 class PostList(ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
